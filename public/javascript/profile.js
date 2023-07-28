2f86b70220dfef830e74fb12fec6f68dbe16cabd
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-07-27 22:43:44 -0400

    line by line study

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -1,48 +1,23 @@
-const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#post-name').value.trim();
-    const needed_funding = document.querySelector('#post-body').value.trim();
-    const description = document.querySelector('#post-desc').value.trim();
-  
-    if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
-        method: 'POST',
-        body: JSON.stringify({ name, post-body, description }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create post');
-      }
-    }
-  };
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    try {
       const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE',
       });
-  
+
       if (response.ok) {
         document.location.replace('/profile');
       } else {
         alert('Failed to delete post');
       }
+    } catch (err) {
+      console.error(err);
+      alert('An error occurred while deleting the post');
     }
-  };
-  
-  document
-    .querySelector('.new-post-form')
-    .addEventListener('submit', newFormHandler);
-  
-  document
-    .querySelector('.post-list')
-    .addEventListener('click', delButtonHandler);
-  
+  }
+};
+
+// Add event listener to delete button
+document.querySelector('.post-list').addEventListener('click', delButtonHandler);
