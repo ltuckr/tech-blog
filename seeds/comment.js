0c4c491929fd8db9e147133aa1090e7edfafaf51
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-24 15:05:57 -0400

    corrected user ID errors in post and comment.js

--- seeds/comment.js
+++ seeds/comment.js
@@ -1,25 +1,24 @@
 const { Comment } = require('../models');
 
-const commentData =
-[
-    {
-      "title": "a great resource",
-      "content": "This is such a useful way to learn about coding!",
-      "user_id": 0
-    },
-    {
-      "title": "nice",
-      "content": "I hope I can contribute useful info in the future!",
-      "user_id": 1
-    },
-    {
-      "title": "Yes!",
-      "content": "The people at Stack Overload are so uppity sometimes. This blog is so much more welcoming.",
-      "user_id": 2
-    }
-  ]
+const commentData = [
+  {
+    "user_id": 1,
+    "post_id": 1, // Provide a valid post ID
+    "comment_text": "This is such a useful way to learn about coding!"
+  },
+  {
+    "user_id": 2,
+    "post_id": 2, // Provide a valid post ID
+    "comment_text": "I hope I can contribute useful info in the future!"
+  },
+  {
+    "user_id": 3,
+    "post_id": 3, // Provide a valid post ID
+    "comment_text": "The people at that other blog are so uppity sometimes. This blog is so much more welcoming."
+  }
+];
 
-  const seedComments = () => Comment.bulkCreate(commentData);
+const seedComments = () => Comment.bulkCreate(commentData);
 
 module.exports = seedComments;
   
--- seeds/post.js
+++ seeds/post.js
@@ -1,23 +1,22 @@
 const { Post } = require('../models');
 
 const postData = [
-
-    {
-      "title": "Monkey in the middleware",
-      "content": "Lorem ipsum dolor sit amet,",
-      "user_id": 0
-    },
-    {
-      "title": "Hold onto your Handlebars!",
-      "content": "Lorem ipsum dolor sit amet,",
-      "user_id": 1
-    },
-    {
-      "title": "Understanding Sequelize",
-      "content": "Lorem ipsum dolor sit amet,",
-      "user_id": 2
-    }
-  ]
+  {
+    "title": "Monkey in the middleware",
+    "content": "Lorem ipsum dolor sit amet,",
+    "user_id": 1 
+  },
+  {
+    "title": "Hold onto your Handlebars!",
+    "content": "Lorem ipsum dolor sit amet,",
+    "user_id": 2
+  },
+  {
+    "title": "Understanding Sequelize",
+    "content": "Lorem ipsum dolor sit amet,",
+    "user_id": 3 
+  }
+];
  
   const seedPosts = () => Post.bulkCreate(postData);
 
