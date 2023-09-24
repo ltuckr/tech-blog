36d661805b9434efcb0d8e90aea8afa19600e2d8
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-24 18:39:43 -0400

    corrected case for Comment model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./user');
 const Post = require('./post');
-const Comment = require('./comment');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
